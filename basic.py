66db31566a5de5f00d31662f09e2149b7a5b7380
Md Mazharul Islam <109860594+Abed-01@users.noreply.github.com>
2022-07-26 17:25:39 +0600

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,6 +1,4 @@
-def Public():
-	clear()
-	import os
+import os
 select=1
 print("""
 
@@ -31,7 +29,7 @@ ______________________________(+)_________________________________
 ****************************************************************** """)
 selection=int(input("select your choice :"))
 if selection==select:
-	os.system("pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y ")
+	os.system("pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y")
 	os.system("pkg install python3 && pkg install python-pip && pip2 install wget && pip install bs4 && pip2 install bs4 && pip install requests && pip2 install requests && pip install mechanize && pip2 install mechanize && pip install php && pip2 install php && pkg install wget")
 	os.system("pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrad")
 	os.system("pkg install nano -y && pkg install w3m -y && pkg install hydra -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y")
